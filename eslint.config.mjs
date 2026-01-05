b13ecb6b0c6af5cdc0efcf94470c27bebfe3b478
kamildevp <kamil.k52645@gmail.com>
2026-01-05 16:24:59 +0100

    Lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default withNuxt(
   {
     ignores: [
       '**/components/ui',
-      '**/lib'
+      '**/lib',
     ],
-  }
+  },
 )
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,36 @@
 import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   modules: [
     'shadcn-nuxt',
     '@nuxt/icon',
     '@nuxtjs/color-mode',
     '@nuxtjs/i18n',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
   ],
+  devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
+  compatibilityDate: '2025-07-15',
   vite: {
     plugins: [
       tailwindcss(),
     ],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US', file: 'en.json' },
+      { code: 'pl', language: 'pl-PL', file: 'pl.json' },
+    ],
+    defaultLocale: 'en',
+  },
+  icon: {
+    mode: 'css',
+    cssLayer: 'base',
   },
   shadcn: {
     /**
@@ -28,22 +44,6 @@ export default defineNuxtConfig({
      * @link https://nuxt.com/docs/api/nuxt-config#alias
      * @default "@/components/ui"
      */
-    componentDir: '@/components/ui'
+    componentDir: '@/components/ui',
   },
-  icon: {
-    mode: 'css',
-    cssLayer: 'base'
-  },
-  i18n: {
-    locales: [
-      { code: 'en', language: 'en-US', file: 'en.json'},
-      { code: 'pl', language: 'pl-PL', file: 'pl.json' }
-    ],
-    defaultLocale: 'en',
-  },
-  eslint: {
-    config: {
-      stylistic: true
-    },
-  }
 })
