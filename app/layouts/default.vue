deb54303ce0e1e973664a37743bd5bf7de5d7b39
kamildevp <kamil.k52645@gmail.com>
2026-01-31 20:05:40 +0100

    Added sonner component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,8 +3,20 @@
     <div>
       <AppHeader />
     </div>
-    <div class="flex-1">
+    <div class="flex-1 relative">
       <slot />
+      <UiToaster
+        position="top-center"
+        rich-colors
+        class="absolute!"
+        :theme="isDark ? 'dark' : 'light'"
+      />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import 'vue-sonner/style.css'
+
+const { isDark } = useDarkSSR()
+</script>
