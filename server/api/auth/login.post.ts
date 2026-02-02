c995353db3d57fc8b23a24ba2e3fe563b552163f
kamildevp <kamil.k52645@gmail.com>
2026-02-03 00:05:07 +0100

    Lint fix

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -11,8 +11,8 @@ export default defineEventHandler(async (event): Promise<AuthState> => {
   setAuthCookies(event, responseData.access_token, responseData.refresh_token)
   const userData = await event.$fetch('/api/users/me', {
     headers: {
-      Cookie: `${accessTokenCookieName}=${responseData.access_token};`
-    }
+      Cookie: `${accessTokenCookieName}=${responseData.access_token};`,
+    },
   })
 
   return {
