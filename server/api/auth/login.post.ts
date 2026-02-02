c3e42dcfeb418b4b0a97b4c37ebcaaab9b5ffd42
kamildevp <kamil.k52645@gmail.com>
2026-02-03 00:04:28 +0100

    Added user data to login endpoint response

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,3 +1,4 @@
+import { accessTokenCookieName } from '~~/server/constants'
 import accessTokens from '~~/server/schemas/auth/accessTokens'
 import successResponse from '~~/server/schemas/response/successResponse'
 import type { AuthState } from '~~/types/auth'
@@ -8,8 +9,14 @@ export default defineEventHandler(async (event): Promise<AuthState> => {
   const responseSchema = successResponse(accessTokens)
   const responseData = responseSchema.parse(response).data
   setAuthCookies(event, responseData.access_token, responseData.refresh_token)
+  const userData = await event.$fetch('/api/users/me', {
+    headers: {
+      Cookie: `${accessTokenCookieName}=${responseData.access_token};`
+    }
+  })
 
   return {
     status: 'authenticated',
+    userData,
   }
 })
