bcf8f67e36135659cef481ce0976c5dcd43275ea
kamildevp <kamil.k52645@gmail.com>
2026-02-05 20:41:06 +0100

    Added custom select component

--- app/components/app/LangSwitch.vue
+++ app/components/app/LangSwitch.vue
@@ -1,42 +1,22 @@
 <template>
   <div>
-    <UiSelect
+    <AppSelect
+      :entries="entries"
+      label-key="name"
+      value-key="value"
+      icon-key="icon"
       :default-value="locale"
-      @update:model-value="(value) => setLocale(value as typeof locale)"
-    >
-      <UiSelectTrigger>
-        <UiSelectValue placeholder="Select language">
-          <div class="flex items-center gap-1 min-w-20">
-            <Icon
-              class="border bg-cover"
-              size="16"
-              :name="`flag-${selectedLocale?.code}-4x3`"
-            />
-            {{ selectedLocale?.name }}
-          </div>
-        </UiSelectValue>
-      </UiSelectTrigger>
-      <UiSelectContent>
-        <UiSelectItem
-          v-for="lang in locales"
-          :key="lang.code"
-          :value="lang.code"
-        >
-          <div class="flex items-center gap-1">
-            <Icon
-              class="border bg-cover"
-              size="16"
-              :name="`flag-${lang.code}-4x3`"
-            />
-            {{ lang.name }}
-          </div>
-        </UiSelectItem>
-      </UiSelectContent>
-    </UiSelect>
+      @value-changed="(value) => setLocale(value)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 const { locale, locales, setLocale } = useI18n()
-const selectedLocale = computed(() => locales.value.find(el => el.code === locale.value))
+
+const entries = computed(() => locales.value.map(locale => ({
+  name: locale.name ?? '',
+  value: locale.code,
+  icon: `flag-${locale.code}-4x3`,
+})))
 </script>
---
+++ app/components/app/Select.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <UiSelect
+      :default-value="defaultValue"
+      @update:model-value="(value) => onValueChanged(value as ET[VK])"
+    >
+      <UiSelectTrigger>
+        <UiSelectValue :placeholder="placeholder">
+          <div
+            v-if="selectedEntry"
+            class="flex items-center gap-1 min-w-20"
+          >
+            <Icon
+              v-if="iconKey && selectedEntry[iconKey]"
+              class="border bg-cover"
+              size="16"
+              :name="selectedEntry[iconKey]"
+            />
+            {{ selectedEntry ? selectedEntry[labelKey] : '' }}
+          </div>
+        </UiSelectValue>
+      </UiSelectTrigger>
+      <UiSelectContent>
+        <UiSelectItem
+          v-for="entry in entries"
+          :key="entry[valueKey]"
+          :value="entry[valueKey]"
+        >
+          <div class="flex items-center gap-1">
+            <Icon
+              v-if="iconKey && entry[iconKey]"
+              class="border bg-cover"
+              size="16"
+              :name="entry[iconKey]"
+            />
+            {{ entry[labelKey] }}
+          </div>
+        </UiSelectItem>
+      </UiSelectContent>
+    </UiSelect>
+  </div>
+</template>
+
+<script setup lang="ts"
+  generic="
+    LK extends string,
+    VK extends string,
+    IK extends string,
+    VT extends string|number,
+    ET extends Entry<LK, VK, IK, VT>
+  "
+>
+export interface Props<
+  LabelKey extends string,
+  ValueKey extends string,
+  IconKey extends string,
+  ValueType extends string | number,
+  EntryType extends Entry<LabelKey, ValueKey, IconKey, ValueType>,
+> {
+  labelKey: LabelKey
+  valueKey: ValueKey
+  iconKey?: IconKey
+  entries: EntryType[]
+  placeholder?: string
+  defaultValue: EntryType[ValueKey]
+}
+
+const props = defineProps<Props<LK, VK, IK, VT, ET>>()
+const defaultEntry = props.entries.find(entry => entry[props.valueKey] === props.defaultValue)
+const selectedValue = ref(defaultEntry ? defaultEntry[props.valueKey] : undefined)
+const selectedEntry = computed(() => props.entries.find(entry => entry[props.valueKey] === selectedValue.value))
+
+const emit = defineEmits<{
+  (e: 'valueChanged', value: ET[VK]): void
+}>()
+
+function onValueChanged(newValue: ET[VK]) {
+  selectedValue.value = newValue
+  emit('valueChanged', newValue)
+}
+</script>
+
+<script lang="ts">
+export type Entry<
+  LabelKey extends string,
+  ValueKey extends string,
+  IconKey extends string,
+  ValueType extends string | number,
+> = {
+  [K in LabelKey]: string
+} & {
+  [K in ValueKey]: ValueType
+} & {
+  [K in IconKey]?: string
+}
+</script>
