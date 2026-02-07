72052d03b5c94fc15396a2a2a693790e16164adc
kamildevp <kamil.k52645@gmail.com>
2026-02-07 15:22:14 +0100

    Added additional languages data to languagePreference schema

--- server/schemas/languagePreference.ts
+++ server/schemas/languagePreference.ts
@@ -1,8 +1,18 @@
 import { z } from 'zod'
 
 export const languages = [
-  'en',
-  'pl',
+  {
+    name: 'Polski',
+    value: 'pl',
+    icon: 'flag-pl-4x3',
+  },
+  {
+    name: 'English',
+    value: 'en',
+    icon: 'flag-gb-4x3',
+  },
 ] as const
 
-export default z.enum(languages)
+export const values = languages.map(entry => entry.value)
+
+export default z.enum(values)
