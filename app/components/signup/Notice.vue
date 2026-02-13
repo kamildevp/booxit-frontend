098e244eeb9995777a2e8cbfd5a866febe0e059f
kamildevp <kamil.k52645@gmail.com>
2026-02-13 22:51:07 +0100

    Added SignupNotice

---
+++ app/components/signup/Notice.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppNoticeCard variant="info">
+    <template #title>
+      {{ $t('components.signup.Notice.title') }}
+    </template>
+    <template #content>
+      {{ $t('components.signup.Notice.content') }}
+    </template>
+    <template #footer>
+      <NuxtLinkLocale
+        to="/"
+      >
+        <UiButton>
+          {{ $t('components.signup.Notice.footer.home_button.text') }}
+        </UiButton>
+      </NuxtLinkLocale>
+    </template>
+  </AppNoticeCard>
+</template>
