a58934c951ded66d700dc7e9e5aae5792f0978c2
kamildevp <kamil.k52645@gmail.com>
2026-02-19 17:46:37 +0100

    Fixed password reset endpoint http method

--- server/api/users/reset-password.patch.ts
+++ server/api/users/reset-password.patch.ts
@@ -3,7 +3,7 @@ import successResponse from '~~/server/schemas/response/successResponse'
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
-  const response = await useAPI(event, 'users/reset-password', 'POST', body)
+  const response = await useAPI(event, 'users/reset-password', 'PATCH', body)
   const responseSchema = successResponse(message)
   responseSchema.parse(response)
 
