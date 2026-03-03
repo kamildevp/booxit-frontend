95c63865806b8e0de2675b52bef9e44ddbfb4219
kamildevp <kamil.k52645@gmail.com>
2026-03-03 01:12:26 +0100

    Added social auth page logic

--- app/components/login/Methods.vue
+++ app/components/login/Methods.vue
@@ -3,6 +3,7 @@
     <UiButton
       variant="outline"
       class="w-full"
+      @click="async () => await handleSocialLoginRequest('google')"
     >
       <Icon
         size="16"
@@ -13,3 +14,53 @@
     </UiButton>
   </div>
 </template>
+
+<script setup lang="ts">
+import { toast } from 'vue-sonner'
+import socialAuthLoginParameters from '~~/server/schemas/auth/socialAuthLoginParameters'
+import type { SocialAuthProvider } from '~~/types/socialAuth'
+
+const route = useRoute()
+const query = route.query
+const { login, getSocialAuthRedirectParameters } = useSocialAuth()
+const { t } = useI18n()
+const localePath = useLocalePath()
+const socialAuthFailed = useState('socialAuthFailed', () => false)
+const redirectTo = ref<string | undefined>(undefined)
+
+await callOnce(async () => {
+  const result = socialAuthLoginParameters.safeParse(query)
+  if (result.success) {
+    await handleSocialLogin(result.data.code, result.data.state)
+  }
+})
+
+async function handleSocialLoginRequest(provider: SocialAuthProvider) {
+  const redirectParameters = await getSocialAuthRedirectParameters(provider, localePath('/'))
+  await navigateTo({
+    path: redirectParameters.url,
+    query: redirectParameters.parameters,
+  }, { external: true })
+}
+
+async function handleSocialLogin(code: string, state: string) {
+  const loginResult = await login(code, state).catch(() => false as const)
+  if (loginResult) {
+    redirectTo.value = loginResult.redirect_url
+  }
+  else {
+    socialAuthFailed.value = true
+  }
+}
+
+onMounted(() => {
+  if (socialAuthFailed.value) {
+    socialAuthFailed.value = false
+    toast.error(t('components.login.Methods.error.login_failed'))
+  }
+})
+
+if (redirectTo.value) {
+  await navigateTo(redirectTo.value)
+}
+</script>
