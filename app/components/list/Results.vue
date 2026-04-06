e75633d68eb6a981649dbdd66d70ca115936507d
kamildevp <kamil.k52645@gmail.com>
2026-04-06 22:40:55 +0200

    Added list results

---
+++ app/components/list/Results.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col gap-4 p-4 overflow-auto">
+    <div class="text-muted-foreground p-1">
+      {{ $t('components.list.Results.count_text', { count: resultsCount }) }}
+    </div>
+    <div
+      v-for="item, indx in items"
+      :key="indx"
+    >
+      <slot :item="item" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" generic="T">
+interface Props {
+  items: T[]
+  resultsCount: number
+}
+defineProps<Props>()
+</script>
