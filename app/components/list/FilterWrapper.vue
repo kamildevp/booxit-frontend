b305ad7ad1debab5affb33a6ca892171ea6fbf8d
kamildevp <kamil.k52645@gmail.com>
2026-04-07 21:20:16 +0200

    Added ListFilterWrapper

---
+++ app/components/list/FilterWrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <UiAccordion
+    collapsible
+    default-value="item"
+  >
+    <UiAccordionItem value="item">
+      <UiAccordionTrigger class="cursor-pointer bg-accent text-accent-foreground hover:no-underline p-2 px-4 text-sm xl:text-lg items-center">
+        <div class="flex items-center gap-2">
+          <Icon
+            v-if="icon"
+            :name="icon"
+            size="24"
+          />
+          {{ $te(title) ? $t(title) : title }}
+        </div>
+      </UiAccordionTrigger>
+      <UiAccordionContent class="p-2">
+        <slot />
+      </UiAccordionContent>
+    </UiAccordionItem>
+  </UiAccordion>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title: string
+  icon?: string
+}
+defineProps<Props>()
+</script>
