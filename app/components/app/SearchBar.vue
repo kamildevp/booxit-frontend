a7e36eae23d22ccf7c992eb4f448f859484e6319
kamildevp <kamil.k52645@gmail.com>
2026-04-08 22:53:51 +0200

    Added SearchBar

---
+++ app/components/app/SearchBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <UiInput
+      v-model="modelValue"
+      :placeholder="placeholder"
+      class="rounded-r-none rounded-l-full focus-visible:ring-1 focus-visible:border-primary"
+    />
+    <UiButton
+      class="rounded-l-none px-4"
+      @click="emit('apply')"
+    >
+      <Icon
+        name="ic:round-search"
+        size="24"
+      />
+    </UiButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  placeholder?: string
+}
+defineProps<Props>()
+const modelValue = defineModel<string>()
+const emit = defineEmits<{
+  (e: 'apply'): void
+}>()
+</script>
