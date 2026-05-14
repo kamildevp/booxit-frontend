2c4a6ec22c965ed152f919e10539dc51b1b8cb0e
kamildevp <kamil.k52645@gmail.com>
2026-05-14 16:12:28 +0200

    Added input variant

--- app/components/ui/input/index.ts
+++ app/components/ui/input/index.ts
@@ -1,1 +1,21 @@
+import type { VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
+
 export { default as Input } from "./Input.vue"
+
+export const inputVariants = cva(
+  "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input w-full min-w-0 rounded-md border bg-transparent px-3 py-1 shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  {
+    variants: {
+      size: {
+        default: "h-9 text-base md:text-sm file:h-7",
+        xl: "h-10 md:h-11 px-4 text-base md:text-lg file:h-9",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+)
+
+export type InputVariants = VariantProps<typeof inputVariants>
